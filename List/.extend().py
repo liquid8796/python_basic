6ed926eba04878d8efd82df9ce0e73532f31c12c
liquid8796 <hanam.tranle.5@gmail.com>
2020-02-07 10:59:47 +0700

    07/02

---
+++ List/.extend().py
@@ -0,0 +1,10 @@
+number=[1,2,3]
+print(number)
+temp=[4,5,6]
+number.extend(temp)
+print(number)
+
+name=["nam","man","thang"]
+print(name)
+name+=["ty","nguyen"]
+print(name)
---
+++ List/.reverse().py
@@ -0,0 +1,5 @@
+number=[1,2,3]
+print(number)
+number.reverse()
+print(number)
+
